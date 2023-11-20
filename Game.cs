54c1b5b10ca04f30b3fb0c42175d33e0d30e7e83
Jan Musioł <jmusiol@biostat.com.pl>
2023-11-20 18:54:57 +0100

    few fixes and comments

--- Game.cs
+++ Game.cs
@@ -11,6 +11,7 @@ namespace WorldOfZuul
     private Room? previousRoom;
     private AsiaRoom? Asia;
     private Room? Hub;
+    private Room? Pacific;
     private AfricaRoom? Africa;
     private Command? command;
 
@@ -28,15 +29,25 @@ namespace WorldOfZuul
 
     private void CreateRooms()
     {
-      Asia = new("Somewhere in Asia", "Asia");
       Hub = new("Hub", "You stand in the Ranger Headquarter’s Council Room.\nAs you look around you feel amazed yet again at how this building constructed underwater has such an amazing view into the ocean and its diverse wildlife from the council. \nAdditionally, you see three screens, each with its mission objective and details. On the left screen you see the details for the Pacific (pacific) deployment. On the right screen you see the Asian (asia) mission. On the middle screen you see the information for the Africa (africa) deployment.");
+
+      Asia = new("Somewhere in Asia", "Asia");
+
       Africa = new("Africa Mission", "Africa", "As you approach the middle screen, the mission description says ”SAFE ENDANGERED KORDOFAN GIRAFFE FROM A WILDFIRE” You scream the keyword ”Africa!”. The floor below you opens, and you fall into the pipe. As you go down, it takes a sharp twist left, and you suddenly find yourself on the comfortable leather chair of a small submarine");
+
+      Pacific = new("Ocean", "You are standing now in the pacific ocean. From now you have to decide either going to the warship or to the submarine.To the east you have to nuke the proachers, to the south you have to cut the proacher's lines, and to the west you have to flip their ship.");
+
 
 
       Asia.SetExit("hub", Hub); //among the hub and other rooms we could use just typos like hub, asia, pacific...
       Africa.SetExit("hub", Hub);
+      Pacific.SetExit("hub", Hub);
+
       Hub.SetExit("asia", Asia); //set exit for asia -> to hub
       Hub.SetExit("africa", Africa);
+      Hub.SetExit("pacific", Pacific);
+
+
 
       currentRoom = Hub;
     }
@@ -85,7 +96,6 @@ namespace WorldOfZuul
             // LoadingAnimation.Loading("Loading");
             Asia.CurrentlyInAsiaRoom(ref currentRoom, ref previousRoom);
             GameConsole.WriteLine("Welcome back to the hub", font: FontTheme.Success);
-            Actions.Move("hub", ref currentRoom, ref previousRoom);
             break;
 
           case var room when currentRoom.Equals(Africa):
@@ -93,6 +103,7 @@ namespace WorldOfZuul
 
             GameConsole.WriteLine("Welcome back to the hub", font: FontTheme.Success);
             break;
+
           default:
             break;
         }
@@ -117,9 +128,11 @@ namespace WorldOfZuul
     /// <returns>returns Command object with command name</returns>
     public static Command AskForCommand()
     {
+      //print message asking for command
       Messages.PrintAskForCommandMessage();
       Command? userCommand;
 
+      //keep asking for command until user inputs valid command
       while ((userCommand = Parser.GetCommand(GameConsole.Input("", breakline: false))) == null)
       {
         Messages.PrintUnknownCommandMessage();
